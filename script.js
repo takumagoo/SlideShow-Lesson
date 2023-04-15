0d710171e75af4c1f8f2b92b8b6906f4207605b1
takumagoo <117556435+takumagoo@users.noreply.github.com>
2023-04-15 06:09:29 +0000

    8章プラグインを導入しよう 完

---
+++ script.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  // オプション設定
+  loop: true,   //最後までスライドしたら最初の画像に戻る
+
+  // ページネーション表示の設定
+  pagination: {
+    el: '.swiper-pagination',   //ページネーションの要素
+  },
+
+  //ナビゲーションボタン（矢印）表示の設定
+  navigation: {
+    nextEl: '.swiper-button-next', //「次へボタン」要素の指定
+    prevEl: '.swiper-button-prev', //「前へボタン」要素の指定
+  }
+
+});
